29df6d34706cd7a53b4f79bfddaced6088bd8cd7
Madhurya-Ramesh <94900616+Madhurya-Ramesh@users.noreply.github.com>
2022-04-11 15:40:30 +0530

    Update vijeth2.py

--- vijeth2.py
+++ vijeth2.py
@@ -20,3 +20,31 @@ r = requests.get(url, headers=headers, auth=(EMAIL, JIRA_API))
 
 
 print(r.json())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# url = "https://chandratech.atlassian.net/rest/api/3/issue/Ap-17"
+# # token = 'EqijvLWIwETIVbQXEgBFEA26'
+# headers = {
+#    "Accept": "application/json",
+#    "Content-Type": "application/json"
+# }
+
+# r = requests.get(url, headers=headers, auth=(EMAIL_CHANDRA, API_CHANDRA))
+
+# #print(r.text)
+# #print(type(r))
+
+
+# print(json.dumps(json.loads(r.text), sort_keys=True, indent=4, separators=(",", ": ")))
