1fbabc9c6ae178f1456b3f76accc66598c28bc53
Madhurya-Ramesh <94900616+Madhurya-Ramesh@users.noreply.github.com>
2022-04-12 14:05:30 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,16 +6,21 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello World!";
+//     private final String message = "Hello World!";
 
-    public App() {}
-
-    public static void main(String[] args) {
-        System.out.println(new App().getMessage());
+    public App() 
+    {
+   System.out.println("hello world");
     }
 
-    private final String getMessage() {
-        return message;
-    }
+//     public static void main(String[] args) {
+//         System.out.println(new App().getMessage());
+//     }
+
+//     private final String getMessage() {
+//         return message;
+//     }
 
 }
+
+
